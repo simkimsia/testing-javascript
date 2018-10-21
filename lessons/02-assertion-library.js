4b6fa6716ef64acb6ce56f7b118e31f9ff2ec5bc
KimSia Sim <kimcity@gmail.com>
2018-10-21 22:08:01 +0800

    Complete lesson on Abstract Test Assertions into a JavaScript Assertion library

---
+++ lessons/02-assertion-library.js
@@ -0,0 +1,31 @@
+const {sum, subtract} = require('../math')
+
+let result, expected
+
+result = sum(3, 7)
+expected = 10
+
+if (expected !== result) {
+    throw new Error(`${result} is not equal to ${expected}`)
+}
+
+result = subtract(3, 7)
+expected = -4
+
+if (expected !== result) {
+    throw new Error(`${result} is not equal to ${expected}`)
+}
+
+function expect(actual) {
+    // return an object
+    return {
+        // with functions such as toBe.. this helps with chaining
+        toBe(expected) {
+            if (actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`)
+            }
+        },
+        toEqual(expected) {},
+        toBeGreaterThan(expected) {},
+    }
+}
